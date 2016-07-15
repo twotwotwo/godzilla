4f7f830d832290d364b940c5f8afb77eeae590a8
Olivier Gagnon <oli.gagnon4418@gmail.com>
2016-07-15 03:25:26 -0400

    slight refactor

--- cmd/godzilla/main.go
+++ cmd/godzilla/main.go
@@ -19,6 +19,8 @@ import (
 	"strings"
 	"sync"
 	"syscall"
+
+	"github.com/hydroflame/godzilla/mutators"
 )
 
 const (
@@ -181,16 +183,16 @@ func main() {
 	}
 
 	// build all the mutators
-	mutators := []Mutator{
-		swapIfElse,
-		ConditionalsBoundaryMutator,
-		MathMutator,
-		MathAssignMutator,
-		VoidCallRemoverMutator,
-		BooleanOperatorsMutator,
+	mtrs := []mutators.Mutator{
+		mutators.SwapIfElse,
+		mutators.ConditionalsBoundaryMutator,
+		mutators.MathMutator,
+		mutators.MathAssignMutator,
+		mutators.VoidCallRemoverMutator,
+		mutators.BooleanOperatorsMutator,
 	}
-	c := make(chan Mutator, len(mutators))
-	for _, mutator := range mutators {
+	c := make(chan mutators.Mutator, len(mtrs))
+	for _, mutator := range mtrs {
 		c <- mutator
 	}
 	close(c)
@@ -213,9 +215,6 @@ func main() {
 	}
 	fmt.Printf("score: %d/%d = %.2f\n", res.total-res.alive, res.total, float64(res.total-res.alive)/float64(res.total))
 }
-
-// Mutator is an operation that can be applied to go source to mutate it.
-type Mutator func(*Visitor, ast.Node, func())
 
 // Result is the data passed to the aggregator to sum the total number of mutant
 // executed and killed for a particular mutation.
@@ -256,18 +255,18 @@ type Visitor struct {
 
 	// this function should make a change to the ast.Node, call the 2nd argument
 	// function and change it back into the original ast.Node.
-	mutator Mutator
+	mutator mutators.Mutator
 
 	coverprofiles []*cover.Profile
 
-	info *types.Info
+	info mutators.TypesInfo
 
 	blocks []cover.ProfileBlock
 }
 
 // Mutate starts mutating the source, it gets the mutators from the given
 // channel.
-func (w worker) Mutate(c chan Mutator, wg *sync.WaitGroup) {
+func (w worker) Mutate(c chan mutators.Mutator, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// Parse the entire package
 	fset := token.NewFileSet()
@@ -347,7 +346,7 @@ func (w worker) Mutate(c chan Mutator, wg *sync.WaitGroup) {
 				pkgs:          spkgs,
 				mutator:       m,
 				coverprofiles: w.coverprofiles,
-				info:          info,
+				info:          mutators.TypesInfo{info},
 				blocks:        blocks,
 			}
 			if strings.HasSuffix(name, "_test.go") {
@@ -456,237 +455,8 @@ func (v *Visitor) Visit(node ast.Node) ast.Visitor {
 	for _, block := range v.blocks {
 		if (block.StartLine < pos.Line || (block.StartLine == pos.Line && pos.Column >= block.StartCol)) &&
 			(block.EndLine > pos.Line || (block.EndLine == pos.Line && pos.Column <= block.EndCol)) {
-			v.mutator(v, node, v.TestMutant)
+			v.mutator(v.info, node, v.TestMutant)
 		}
 	}
 	return v
 }
-
-// VoidCallRemoverMutator removes calls to void function/methods
-func VoidCallRemoverMutator(v *Visitor, node ast.Node, testMutant func()) {
-	if block, ok := node.(*ast.BlockStmt); ok {
-		for i, stmt := range block.List {
-			if expr, ok := stmt.(*ast.ExprStmt); ok {
-				if v, ok := v.info.Types[expr.X]; ok {
-					if v.IsVoid() {
-						mutation := make([]ast.Stmt, len(block.List))
-						copy(mutation, block.List)
-						mutation = mutation[:i+copy(mutation[i:], mutation[i+1:])]
-						old := block.List
-						block.List = mutation
-						testMutant()
-						block.List = old
-					}
-				}
-			}
-		}
-	}
-	/*
-		Types:      make(map[ast.Expr]types.TypeAndValue),
-		Defs:       make(map[*ast.Ident]types.Object),
-		Uses:       make(map[*ast.Ident]types.Object),
-		Implicits:  make(map[ast.Node]types.Object),
-		Selections: make(map[*ast.SelectorExpr]*types.Selection),
-		Scopes:     make(map[ast.Node]*types.Scope),
-	*/
-}
-
-// swapIfElse swaps an ast node if body with the following else statement, if it
-// exists, it will not swap the else if body of an if/else if node.
-func swapIfElse(v *Visitor, node ast.Node, testMutant func()) {
-	// if its an if statement node
-	if ifstmt, ok := node.(*ast.IfStmt); ok {
-		// if theres an else
-		if ifstmt.Else != nil {
-			// if the else is not part of a elseif
-			if el, ok := ifstmt.Else.(*ast.BlockStmt); ok {
-				// swap their body
-				ifstmt.Else = ifstmt.Body
-				ifstmt.Body = el
-				// test that mutant
-				testMutant()
-				// swap back
-				ifstmt.Body = ifstmt.Else.(*ast.BlockStmt)
-				ifstmt.Else = el
-			}
-		}
-	}
-}
-
-var conditionalsBoundaryMutatorTable = map[token.Token]token.Token{
-	token.LSS: token.LEQ,
-	token.LEQ: token.LSS,
-	token.GTR: token.GEQ,
-	token.GEQ: token.GTR,
-}
-
-// ConditionalsBoundaryMutator performs
-//	<  to <=
-//	<= to <
-//	>  to >=
-//	>= to >
-func ConditionalsBoundaryMutator(v *Visitor, node ast.Node, testMutant func()) {
-	if expr, ok := node.(*ast.BinaryExpr); ok {
-		old := expr.Op
-		op, ok := conditionalsBoundaryMutatorTable[expr.Op]
-		if !ok {
-			return
-		}
-		expr.Op = op
-		testMutant()
-		expr.Op = old
-	}
-}
-
-var mathMutatorTable = map[token.Token]token.Token{
-	token.ADD: token.SUB,
-	token.SUB: token.ADD,
-
-	token.MUL: token.QUO,
-	token.QUO: token.MUL,
-
-	token.REM: token.MUL,
-
-	token.AND: token.OR,
-	token.OR:  token.AND,
-
-	token.XOR: token.AND,
-
-	token.SHL: token.SHR,
-	token.SHR: token.SHL,
-}
-
-// MathMutator swaps various mathematical operators
-//	+   to -
-//	-   to +
-//	*   to /
-//	/   to *
-//	%   to *
-//	&   to |
-//	|   to &
-//	^   to &
-//	<<  to >>
-//	>>  to <<
-func MathMutator(v *Visitor, node ast.Node, testMutant func()) {
-	if expr, ok := node.(*ast.BinaryExpr); ok {
-		old := expr.Op
-		op, ok := mathMutatorTable[expr.Op]
-		if !ok {
-			return
-		}
-		expr.Op = op
-		testMutant()
-		expr.Op = old
-	}
-}
-
-var booleanMutatorTable = map[token.Token]token.Token{
-	token.LAND: token.LOR,
-	token.LOR:  token.LAND,
-}
-
-// BooleanOperatorsMutator swaps various mathematical operators.
-//	&&	to	||
-//	||	to	&&
-func BooleanOperatorsMutator(v *Visitor, node ast.Node, testMutant func()) {
-	if expr, ok := node.(*ast.BinaryExpr); ok {
-		old := expr.Op
-		op, ok := booleanMutatorTable[expr.Op]
-		if !ok {
-			return
-		}
-		expr.Op = op
-		testMutant()
-		expr.Op = old
-	}
-}
-
-var mathAssignementMutatorTable = map[token.Token]token.Token{
-	token.ADD_ASSIGN: token.SUB_ASSIGN,
-	token.SUB_ASSIGN: token.ADD_ASSIGN,
-
-	token.MUL_ASSIGN: token.QUO_ASSIGN,
-	token.QUO_ASSIGN: token.MUL_ASSIGN,
-
-	token.REM_ASSIGN: token.MUL_ASSIGN,
-
-	token.AND_ASSIGN: token.OR_ASSIGN,
-	token.OR_ASSIGN:  token.AND_ASSIGN,
-
-	token.XOR_ASSIGN: token.AND_ASSIGN,
-
-	token.SHL_ASSIGN: token.SHR_ASSIGN,
-	token.SHR_ASSIGN: token.SHL_ASSIGN,
-}
-
-// MathAssignMutator acts like MathMutator but on assignements.
-func MathAssignMutator(v *Visitor, node ast.Node, testMutant func()) {
-	if assign, ok := node.(*ast.AssignStmt); ok {
-		old := assign.Tok
-		op, ok := mathAssignementMutatorTable[assign.Tok]
-		if !ok {
-			return
-		}
-		assign.Tok = op
-		testMutant()
-		assign.Tok = old
-	}
-}
-
-var negateConditionalsMutatorTable = map[token.Token]token.Token{
-	token.EQL: token.NEQ,
-	token.NEQ: token.EQL,
-
-	token.LSS: token.GEQ,
-	token.GEQ: token.LSS,
-
-	token.GTR: token.LEQ,
-	token.LEQ: token.GTR,
-}
-
-// NegateConditionalsMutator negates some boolean checks
-func NegateConditionalsMutator(v *Visitor, node ast.Node, testMutant func()) {
-	if expr, ok := node.(*ast.BinaryExpr); ok {
-		old := expr.Op
-		op, ok := negateConditionalsMutatorTable[expr.Op]
-		if !ok {
-			return
-		}
-		expr.Op = op
-		testMutant()
-		expr.Op = old
-	}
-}
-
-// Increments Mutator
-/*
-++
---
-*/
-
-// Invert Negatives Mutator
-/*
-i => -i
-*/
-
-// Math Mutator
-/*
- */
-
-// Return Values Mutator
-/*
-boolean         replace the unmutated return value true with false and replace the unmutated return value false with true
-int byte short  if the unmutated return value is 0 return 1, otherwise mutate to return value 0
-long            replace the unmutated return value x with the result of x+1
-float double    replace the unmutated return value x with the result of -(x+1.0) if x is not NAN and replace NAN with 0
-Object          replace non-null return values with null and throw a java.lang.RuntimeException if the unmutated method would return null
-*/
-
-// Inline constant mutator
-/*
-boolean             replace the unmutated value true with false and replace the unmutated value false with true
-integer byte short  replace the unmutated value 1 with 0, -1 with 1, 5 with -1 or otherwise increment the unmutated value by one. 1
-long                replace the unmutated value 1 with 0, otherwise increment the unmutated value by one.
-float               replace the unmutated values 1.0 and 2.0 with 0.0 and replace any other value with 1.0 2
-double              replace the unmutated value 1.0 with 0.0 and replace any other value with 1.0 3
-*/
---
+++ mutators/mutators.go
@@ -0,0 +1,236 @@
+package mutators
+
+import (
+	"go/ast"
+	"go/token"
+	"go/types"
+)
+
+// Mutator is an operation that can be applied to go source to mutate it.
+type Mutator func(TypesInfo, ast.Node, func())
+
+// TypesInfo is a struct that holds all the type information.
+type TypesInfo struct {
+	*types.Info
+}
+
+// VoidCallRemoverMutator removes calls to void function/methods
+func VoidCallRemoverMutator(v TypesInfo, node ast.Node, testMutant func()) {
+	if block, ok := node.(*ast.BlockStmt); ok {
+		for i, stmt := range block.List {
+			if expr, ok := stmt.(*ast.ExprStmt); ok {
+				if v, ok := v.Types[expr.X]; ok {
+					if v.IsVoid() {
+						mutation := make([]ast.Stmt, len(block.List))
+						copy(mutation, block.List)
+						mutation = mutation[:i+copy(mutation[i:], mutation[i+1:])]
+						old := block.List
+						block.List = mutation
+						testMutant()
+						block.List = old
+					}
+				}
+			}
+		}
+	}
+}
+
+// SwapIfElse swaps an ast node if body with the following else statement, if it
+// exists, it will not swap the else if body of an if/else if node.
+func SwapIfElse(v TypesInfo, node ast.Node, testMutant func()) {
+	// if its an if statement node
+	if ifstmt, ok := node.(*ast.IfStmt); ok {
+		// if theres an else
+		if ifstmt.Else != nil {
+			// if the else is not part of a elseif
+			if el, ok := ifstmt.Else.(*ast.BlockStmt); ok {
+				// swap their body
+				ifstmt.Else = ifstmt.Body
+				ifstmt.Body = el
+				// test that mutant
+				testMutant()
+				// swap back
+				ifstmt.Body = ifstmt.Else.(*ast.BlockStmt)
+				ifstmt.Else = el
+			}
+		}
+	}
+}
+
+var conditionalsBoundaryMutatorTable = map[token.Token]token.Token{
+	token.LSS: token.LEQ,
+	token.LEQ: token.LSS,
+	token.GTR: token.GEQ,
+	token.GEQ: token.GTR,
+}
+
+// ConditionalsBoundaryMutator performs
+//	<  to <=
+//	<= to <
+//	>  to >=
+//	>= to >
+func ConditionalsBoundaryMutator(v TypesInfo, node ast.Node, testMutant func()) {
+	if expr, ok := node.(*ast.BinaryExpr); ok {
+		old := expr.Op
+		op, ok := conditionalsBoundaryMutatorTable[expr.Op]
+		if !ok {
+			return
+		}
+		expr.Op = op
+		testMutant()
+		expr.Op = old
+	}
+}
+
+var mathMutatorTable = map[token.Token]token.Token{
+	token.ADD: token.SUB,
+	token.SUB: token.ADD,
+
+	token.MUL: token.QUO,
+	token.QUO: token.MUL,
+
+	token.REM: token.MUL,
+
+	token.AND: token.OR,
+	token.OR:  token.AND,
+
+	token.XOR: token.AND,
+
+	token.SHL: token.SHR,
+	token.SHR: token.SHL,
+}
+
+// MathMutator swaps various mathematical operators
+//	+   to -
+//	-   to +
+//	*   to /
+//	/   to *
+//	%   to *
+//	&   to |
+//	|   to &
+//	^   to &
+//	<<  to >>
+//	>>  to <<
+func MathMutator(v TypesInfo, node ast.Node, testMutant func()) {
+	if expr, ok := node.(*ast.BinaryExpr); ok {
+		old := expr.Op
+		op, ok := mathMutatorTable[expr.Op]
+		if !ok {
+			return
+		}
+		expr.Op = op
+		testMutant()
+		expr.Op = old
+	}
+}
+
+var booleanMutatorTable = map[token.Token]token.Token{
+	token.LAND: token.LOR,
+	token.LOR:  token.LAND,
+}
+
+// BooleanOperatorsMutator swaps various mathematical operators.
+//	&&	to	||
+//	||	to	&&
+func BooleanOperatorsMutator(v TypesInfo, node ast.Node, testMutant func()) {
+	if expr, ok := node.(*ast.BinaryExpr); ok {
+		old := expr.Op
+		op, ok := booleanMutatorTable[expr.Op]
+		if !ok {
+			return
+		}
+		expr.Op = op
+		testMutant()
+		expr.Op = old
+	}
+}
+
+var mathAssignementMutatorTable = map[token.Token]token.Token{
+	token.ADD_ASSIGN: token.SUB_ASSIGN,
+	token.SUB_ASSIGN: token.ADD_ASSIGN,
+
+	token.MUL_ASSIGN: token.QUO_ASSIGN,
+	token.QUO_ASSIGN: token.MUL_ASSIGN,
+
+	token.REM_ASSIGN: token.MUL_ASSIGN,
+
+	token.AND_ASSIGN: token.OR_ASSIGN,
+	token.OR_ASSIGN:  token.AND_ASSIGN,
+
+	token.XOR_ASSIGN: token.AND_ASSIGN,
+
+	token.SHL_ASSIGN: token.SHR_ASSIGN,
+	token.SHR_ASSIGN: token.SHL_ASSIGN,
+}
+
+// MathAssignMutator acts like MathMutator but on assignements.
+func MathAssignMutator(v TypesInfo, node ast.Node, testMutant func()) {
+	if assign, ok := node.(*ast.AssignStmt); ok {
+		old := assign.Tok
+		op, ok := mathAssignementMutatorTable[assign.Tok]
+		if !ok {
+			return
+		}
+		assign.Tok = op
+		testMutant()
+		assign.Tok = old
+	}
+}
+
+var negateConditionalsMutatorTable = map[token.Token]token.Token{
+	token.EQL: token.NEQ,
+	token.NEQ: token.EQL,
+
+	token.LSS: token.GEQ,
+	token.GEQ: token.LSS,
+
+	token.GTR: token.LEQ,
+	token.LEQ: token.GTR,
+}
+
+// NegateConditionalsMutator negates some boolean checks
+func NegateConditionalsMutator(v TypesInfo, node ast.Node, testMutant func()) {
+	if expr, ok := node.(*ast.BinaryExpr); ok {
+		old := expr.Op
+		op, ok := negateConditionalsMutatorTable[expr.Op]
+		if !ok {
+			return
+		}
+		expr.Op = op
+		testMutant()
+		expr.Op = old
+	}
+}
+
+// Increments Mutator
+/*
+++
+--
+*/
+
+// Invert Negatives Mutator
+/*
+i => -i
+*/
+
+// Math Mutator
+/*
+ */
+
+// Return Values Mutator
+/*
+boolean         replace the unmutated return value true with false and replace the unmutated return value false with true
+int byte short  if the unmutated return value is 0 return 1, otherwise mutate to return value 0
+long            replace the unmutated return value x with the result of x+1
+float double    replace the unmutated return value x with the result of -(x+1.0) if x is not NAN and replace NAN with 0
+Object          replace non-null return values with null and throw a java.lang.RuntimeException if the unmutated method would return null
+*/
+
+// Inline constant mutator
+/*
+boolean             replace the unmutated value true with false and replace the unmutated value false with true
+integer byte short  replace the unmutated value 1 with 0, -1 with 1, 5 with -1 or otherwise increment the unmutated value by one. 1
+long                replace the unmutated value 1 with 0, otherwise increment the unmutated value by one.
+float               replace the unmutated values 1.0 and 2.0 with 0.0 and replace any other value with 1.0 2
+double              replace the unmutated value 1.0 with 0.0 and replace any other value with 1.0 3
+*/
